3c2906039736b0362c8ad0db038c0866ab61c936
buloshkagit config --global user.name buloshkagit config --global user.email <to.advanced@yandex.ru>
2024-03-12 22:34:38 +0300

    lol

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+$('.slider__inner').slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    infinite: false,
+});
+$('input, select').styler();
+
+    
+});
